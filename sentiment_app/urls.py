6ff42f23a5e3fc8a0c4e19575a93561f445d3491
NisheetD <nisheet.das@curefit.com>
2025-04-20 02:54:21 -0400

    harley theme init

--- sentiment_app/urls.py
+++ sentiment_app/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', include('sentiment_analyzer.urls')),
 ]
 
-# Add media URL patterns
+# Add media and static URL patterns for development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
